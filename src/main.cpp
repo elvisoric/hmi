31b595029d7a7c23d241889a29ba29d04a5e5281
Elvis Oric <elvis.oric90@gmail.com>
2018-10-01 18:21:35 +0200

    Add glfw as dependency and include_direcories in meson build script. Add glad.cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
+#include <DisplayManager.h>
 #include <iostream>
 
 int main() {
   std::cout << "Hello from main" << std::endl;
+  std::cout << nrg::DisplayManager::screenWidth;
+  std::cout << nrg::DisplayManager::screenHeight;
   return 0;
 }
